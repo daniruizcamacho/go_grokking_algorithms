e108fb67cc1d8f7406e900a447342455460736c3
Daniel Ruiz Camacho <daniel.ruizcamacho@gmail.com>
2021-04-04 18:37:14 +0200

    Quicksort last element as pivot. Recursive

--- chapter4/quicksort/quicksort.go
+++ chapter4/quicksort/quicksort.go
@@ -25,3 +25,29 @@ func quicksortFirstElementPivot(values []int) []int {
 	result = append(result, quicksortFirstElementPivot(greater)...)
 	return result
 }
+
+func quicksortLastElementPivot(values []int) []int {
+	var less, greater, result []int
+	valuesCopy := make([]int, len(values))
+	copy(valuesCopy, values)
+
+	if len(valuesCopy) < 2 {
+		return valuesCopy
+	}
+
+	pivot := valuesCopy[len(valuesCopy)-1]
+	for _, value := range valuesCopy[:len(valuesCopy)-1] {
+		if value <= pivot {
+			less = append(less, value)
+		}
+
+		if value > pivot {
+			greater = append(greater, value)
+		}
+	}
+
+	result = append(result, quicksortLastElementPivot(less)...)
+	result = append(result, pivot)
+	result = append(result, quicksortLastElementPivot(greater)...)
+	return result
+}
--- chapter4/quicksort/quicksort_test.go
+++ chapter4/quicksort/quicksort_test.go
@@ -37,3 +37,36 @@ func TestQuicksortFirstElementPivot(t *testing.T) {
 		})
 	}
 }
+
+func TestQuicksortLastElementPivot(t *testing.T) {
+	tests := map[string]struct {
+		arg  []int
+		want []int
+	}{
+		"Base case empty slice": {
+			arg:  []int{},
+			want: []int{},
+		},
+		"Base case one element slice": {
+			arg:  []int{1},
+			want: []int{1},
+		},
+		"Sorted slice": {
+			arg:  []int{1, 2, 3, 4, 5},
+			want: []int{1, 2, 3, 4, 5},
+		},
+		"Unsorted slice": {
+			arg:  []int{5, 3, 1, 2, 4},
+			want: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := quicksortLastElementPivot(tc.arg)
+
+			if false == reflect.DeepEqual(got, tc.want) {
+				t.Errorf("Want %v; got %v", tc.want, got)
+			}
+		})
+	}
+}
